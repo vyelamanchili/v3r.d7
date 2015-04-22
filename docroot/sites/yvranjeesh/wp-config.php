9b760e7f0004abb316d82ae6533b659765e95925
Ranjeesh Yelamanchili <venkatram.yelamanchili@strayer.edu>
2015-04-22 00:25:09 -0400

    Wordpress settings Updated to localhost
    Former-commit-id: ba3e3ef8b95f4981b80c769b60b710b3f3022072

--- docroot/sites/yvranjeesh/wp-config.php
+++ docroot/sites/yvranjeesh/wp-config.php
@@ -25,7 +25,7 @@ define('DB_USER', 'v3r');
 define('DB_PASSWORD', 'Ranjureetu59');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost);
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
